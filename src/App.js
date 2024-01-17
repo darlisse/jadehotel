4689c5457591abf51b3f3039bbe3df73fe6ff91e
darlisse <darlissecayra.dumaliang@tup.edu.ph>
2024-01-17 16:42:53 +0800

    modified
    added pages and components folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/header";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact Component={Home} />
+          {/*<Route path="/rooms" Component={Rooms} />*/}
+          <Route path="/About" exact Component={About} />
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from "react";
+//import { FaBars, FaTimes } from "react-icons/fa";
+import "./header.css";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <h3>Logo</h3>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/rooms">Rooms</Link>
+        <Link to="/About">About</Link>
+      </nav>
+    </header>
+  );
+}
+export default Header;
