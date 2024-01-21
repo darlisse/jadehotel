e3527ed28c367505335b6b4f94befd8e44a3a3f1
JudeBanar <judemichael.banar@tup.edu.ph>
2024-01-21 11:19:22 +0800

    Created divisions and css file

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,7 @@ function Header() {
       <h3>Logo</h3>
       <nav>
         <Link to="/">Home</Link>
-        <Link to="/rooms">Rooms</Link>
+        <Link to="/Details">Rooms</Link>
         <Link to="/About">About</Link>
       </nav>
     </header>
--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Details = () => {
-    return <h1>holabels</h1>;
-  };
+
+  return (
+
+    <div className="room-details-container">
+      <div className="picture-container">
+        <h1>Hello din</h1>
+      </div>
+      <div className="details-container">
+        <h1>Hello din</h1>
+      </div>
+    </div>
+    
+  );
+    
+};
+
+
   
-  export default Details;
+export default Details;
