084b6e6ae770008dacdf8bab8f2e0026b02c3843
darlisse <darlissecayra.dumaliang@tup.edu.ph>
2024-01-22 05:01:28 +0800

    added images

--- src/pages/About.js
+++ src/pages/About.js
@@ -44,6 +44,7 @@ const About = () => {
         <div className="card">
           <div className="card_part card_part-one"></div>
           <div className="card_part card_part-two"></div>
+          <div className="card_part card_part-three"></div>
         </div>
       </div>
     </section>
