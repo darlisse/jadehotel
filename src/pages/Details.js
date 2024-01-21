0bcca9bd8845eb905868028265b11a52fd830d7a
JudeBanar <judemichael.banar@tup.edu.ph>
2024-01-21 13:54:18 +0800

    Added room description

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -11,8 +11,15 @@ const Details = () => {
       </div>
       <div className="details-container">
         <h1 className="room-name">Single room</h1>
-        <article>
-          
+        <article className="room-description">
+        The Single Room in our hotel is a cozy haven designed for solo 
+        travelers. Furnished with a comfortable bed and modern amenities, 
+        it offers a tranquil space to unwind. The ensuite bathroom provides 
+        convenience and privacy, and guests can enjoy complimentary toiletries. 
+        With a well-appointed workspace, high-speed internet, and a flat-screen 
+        TV, the room combines functionality with comfort. Room service and 
+        access to hotel facilities ensure a delightful stay for both business 
+        and leisure travelers.
         </article>
       </div>
     </div>
