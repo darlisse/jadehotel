d8da72bdc6e38c19ae8f8b761f71513907f41d4d
JudeBanar <judemichael.banar@tup.edu.ph>
2024-01-22 03:15:33 +0800

    Linked the Details file to Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -31,67 +31,85 @@ const Home = () => {
           <div className="Room-details-container">
             <div className="Room-details-container">
               <div className="Room-container">
-                <article>
-                  <img
-                    src={twin} alt="Twin"
-                    className="icon"
-                  />
-                  <div>
-                    <h3>Twin Room</h3>
-                    <p>Two separate single beds placed in the same room</p>
-                  </div>
-                  <Link to="/Details">View</Link>
+              
+                <article> 
+                  <Link to="/Details">
+                    <img
+                      src={twin} alt="Twin"
+                      className="icon"
+                    />
+                    <div>
+                      <h3>Twin Room</h3>
+                      <p>Two separate single beds placed in the same room</p>
+                    </div>
+                  </Link>
                 </article>
+                
                 <article>
-              <img
-                src={double} alt="Double Bed"
-                class="icon"
-              />
-              <div>
-                <h3>Double Room</h3>
-                <p>A bed larger than a single bed, typically designed for two people.</p>
-              </div>
-            </article>
-            <article>
-              <img
-                src={suite} alt="Suit"
-                class="icon"
-              />
-              <div>
-                <h3>Suite</h3>
-                <p>A larger and more luxurious room with a separate living or sitting area. </p>
-              </div>
-            </article>
-            <article>
-              <img
-                src={executive} alt="Executive"
-                class="icon"
-              />
-              <div>
-                <h3>Executive Room</h3>
-                <p>A room with added amenities and services catering to business travelers.</p>
-              </div>
-            </article>
-            <article>
-              <img
-                src={presidential} alt="Presidential"
-                class="icon"
-              />
-              <div>
-                <h3>Presidential Suite</h3>
-                <p>The highest-tier suite in some hotels, offering the most luxurious amenities and often reserved for VIP guests or special occasions.</p>
-              </div>
-            </article>
-            <article>
-              <img
-                src={penthouse} alt="Penthouse"
-                class="icon"
-              />
-              <div>
-                <h3>Penthouse</h3>
-                <p>These rooms are high-end, big rooms- sometimes taking up the entire top floor of a hotel – and come with the ultimate luxury amenities.</p>
-              </div>
-            </article>
+                  <Link to="/Details-2">
+                    <img
+                      src={double} alt="Double Bed"
+                      class="icon"
+                    />
+                    <div>
+                      <h3>Double Room</h3>
+                      <p>A bed larger than a single bed, typically designed for two people.</p>
+                    </div>
+                    </Link>
+                </article>
+
+                <article>
+                  <Link to="/Details-3">
+                    <img
+                    src={suite} alt="Suit"
+                    class="icon"
+                    />
+                    <div>
+                      <h3>Suite</h3>
+                      <p>A larger and more luxurious room with a separate living or sitting area. </p>
+                    </div>
+                  </Link>
+                 </article>
+
+                <article>
+                  <Link to="/Details-4">
+                    <img
+                      src={executive} alt="Executive"
+                      class="icon"
+                    />
+                    <div>
+                      <h3>Executive Room</h3>
+                      <p>A room with added amenities and services catering to business travelers.</p>
+                    </div>
+                  </Link>
+                </article>
+
+                <article>
+                  <Link to="/Details-5">
+                    <img
+                      src={presidential} alt="Presidential"
+                      class="icon"
+                    />
+                    <div>
+                      <h3>Presidential Suite</h3>
+                      <p>The highest-tier suite in some hotels, offering the most luxurious amenities and often reserved for VIP guests or special occasions.</p>
+                    </div>
+                  </Link>
+                </article>
+
+                <article>
+                  <Link to="/Details-6">
+                    <img
+                      src={penthouse} alt="Penthouse"
+                      class="icon"
+                    />
+                    <div>
+                      <h3>Penthouse</h3>
+                      <p>These rooms are high-end, big rooms- sometimes taking up the entire top floor of a hotel – and come with the ultimate luxury amenities.</p>
+                    </div>
+                  </Link>
+                </article>  
+
               </div>
             </div>
           </div>
